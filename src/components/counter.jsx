92e34691373627cf67b52434c79fc313a10f91a0
Timi-cell <adedayotimilehin10@gmail.com>
2021-05-22 00:02:53 +0100

    did a lil bit of react today

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,30 +5,50 @@ class  Counter extends Component {
     //state contains any data that a specific component needs
     //the return keyword can only return one element
   state = { 
-      count: 1
+      count: 1,
+
   };
+  handleIncrement = () => {
+     this.setState({ count: this.state.count + 1});
+  }
+
+//   renderTags() {
+//       if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+//       return (
+//         <ul>
+//           {this.state.tags.map((tag) => (
+//             <li key={tag}>{tag}</li>
+//           ))}
+//         </ul>
+//       );
+//   }
+
+// constructor() {
+//     super();
+//     this.handleIncrement = this.handleIncrement.bind(this);
+// }
 
     render() {
 
         return (
           <div>
- 
-            <span className = {this.getBadgeClasses()}>{this.formatCount() }</span>
-            <button className = "btn btn-secondary btn-sm">Increment</button>
-          </div>
+              {/* {this.state.tags.length === 0 && 'Please create a new tag!'}
+              {this.renderTags()} */}
+    <span className = {this.getBadgeClasses()}> { this.formatCount()}</span>
+    <button onClick = {this.handleIncrement} className = "btn btn-secondary btn-sm">Increment</button>
+         </div>
         );
     }
 
-    getBadgeClasses()
-    {
+    getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += this.state.count === 0 ? "warning" : 'primary';
+        classes += this.state.count === 0 ? "warning" : "primary";
         return classes;
     }
 
     formatCount() {
         const {count} = this.state;
-        return count === 0 ? 'Zero' : count;
+         return count === 0 ? 'Zero' : count;
     }
 }
  
