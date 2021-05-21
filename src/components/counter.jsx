2f66c0fc4267b7b5017ec07112cd1390b1ec0e27
Timi-cell <adedayotimilehin10@gmail.com>
2021-05-21 23:07:09 +0100

    confirm

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 
-class  extends Component {
-    state = {  }
-    render() { 
-        return (  );
+
+class  Counter extends Component {
+    //state contains any data that a specific component needs
+    //the return keyword can only return one element
+  state = { 
+      count: 0,
+
+  };
+    render() {
+        return (
+          <div>
+ 
+            <span className = "badge badge-primary m-2">{this.formatCount() }</span>
+            <button className = "btn btn-secondary btn-sm">Increment</button>
+          </div>
+        );
+    }
+
+    formatCount() {
+        const {count} = this.state;
+        return count === 0 ? 'Zero' : count;
     }
 }
  
-export default ;
+export default Counter;
